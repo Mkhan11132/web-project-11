ec60394f9b05f9f6a7f80e9f9a7c22ffde7473d6
OpenOrgDeveloper <developer@openorg.us>
2026-04-16 20:26:01 +0500

    Fix: Correct Clever-Cloud MySQL password

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const DB_CONFIG = {
   host: process.env.DB_HOST || 'bldm637x6gd2fsalwo71-mysql.services.clever-cloud.com',
   port: parseInt(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER || 'ukqlqkghpqbt7mrh',
-  password: process.env.DB_PASSWORD || 'ukqlqkghpqbt7mrh',
+  password: process.env.DB_PASSWORD || 'Hl321SPpmbZWxzpVftlm',
   database: process.env.DB_NAME || 'bldm637x6gd2fsalwo71',
   waitForConnections: true,
   connectionLimit: 5,
